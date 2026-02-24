3c10d3fefb0d6196d83f8f062adac6c3853b0aef
priyabrata <priyabratabehera140@gmail.com>
2026-02-23 23:17:31 -0600

    Added some problem in Two Pointers

---
+++ Two Pointers/TwoSum.py
@@ -0,0 +1,21 @@
+from typing import List
+
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        left, right = 0, len(numbers) - 1
+        
+        while left < right:
+            curSum = numbers[left] + numbers[right]
+
+            if curSum > target:
+                right -= 1
+            elif curSum < target:
+                left += 1
+            else:
+                return [left + 1, right + 1]
+        return []
+    
+
+    
+sol = Solution()
+print(sol.twoSum([1,2,3,4], 3))
