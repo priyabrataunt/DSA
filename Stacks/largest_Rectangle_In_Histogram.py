614d6b74ff9c148169d3c259335aa395c7f6c517
priyabrata <priyabratabehera140@gmail.com>
2026-03-05 00:04:07 -0600

    Included a Hard Problem

---
+++ Stacks/largest_Rectangle_In_Histogram.py
@@ -0,0 +1,21 @@
+def largest_Rectangle(heights):
+    stack = []
+    max_area = 0
+
+    for current_index, h in enumerate(heights):
+        start = current_index
+        while stack and stack[-1][1] > h:
+            start_index, height = stack.pop()
+            area = height * (current_index - start_index)
+            max_area = max(max_area, area)
+            start = start_index
+        stack.append((start, h))
+
+    
+    for i, h in stack:
+        area = h * (len(heights) - i)
+        max_area = max(max_area, area)
+    return max_area
+
+
+print(largest_Rectangle([7,1,7,2,2,4]))
